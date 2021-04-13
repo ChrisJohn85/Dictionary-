93237e4332a6d7a1f081c7ce22fbb7669558cae6
ChrisJohn85 <62530178+ChrisJohn85@users.noreply.github.com>
2021-04-14 00:09:48 +1000

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+
+
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+
+
+
+}
+print(monthConversions.get("Luv", "Not a Valid Key"))
+
+
+
+# .get specifies the default value if a invalid key is entered
